3cf945553e6e0b577a09fce2b909d016e1bf09e3
Praveen Kumar <142908017+praveenkr122@users.noreply.github.com>
2024-04-11 10:20:15 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://praveenkumar0101024:841406a@A@book-store-mern.znuoaza.mongodb.net/Book-Collection?retryWrites=true&w=majority&appName=Book-Store-MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
